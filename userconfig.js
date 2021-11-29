92063a0b9486423af7229dd285d77352af8cd1a3
vlagh3 <82247523+vlagh3@users.noreply.github.com>
2021-11-29 18:36:32 +0200

    changed conf with my preferences

--- userconfig.js
+++ userconfig.js
@@ -5,18 +5,17 @@ const GLOBAL_CONFIG = new Config({
     refreshIn: 60
   },
   temperature: {
-    location: 'New York',
+    location: 'Bucharest',
     scale: 'C'
   },
-  clock: 'h:i p',
+  clock: 'h:i p - m b',
   search: {
     engines: {
       g: ['https://google.com/search?q=', 'Google'],
-      i: ['https://ixquick.com/do/search?q=', 'Ixquick'],
-      d: ['https://duckduckgo.com/html?q=', 'DuckDuckGo'],
+      d: ['https://duckduckgo.com/?q=', 'DuckDuckGo'],
       y: ['https://youtube.com/results?search_query=', 'Youtube'],
       w: ['https://en.wikipedia.org/w/index.php?search=', 'Wikipedia']
     }
   },
-  disabled: []
+  disabled: ["todo-list"]
 });
